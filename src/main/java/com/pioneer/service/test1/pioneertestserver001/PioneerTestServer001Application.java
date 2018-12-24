674d64d5ec5669bf4b0304700118f97783b05388
Ben Gonzales <bgonzales20001@gmail.com>
2018-12-24 16:58:39 -0600

    Init repo

---
+++ src/main/java/com/pioneer/service/test1/pioneertestserver001/PioneerTestServer001Application.java
@@ -0,0 +1,14 @@
+package com.pioneer.service.test1.pioneertestserver001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PioneerTestServer001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PioneerTestServer001Application.class, args);
+	}
+
+}
+
